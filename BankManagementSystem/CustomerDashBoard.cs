3d0277e06774a6192aea2b1d6d7cd3d83ed77e19
GauravTrianz1706 <gaurav.sharma@trianz.com>
2026-04-21 16:06:54 +0530

    Update CustomerDashBoard.cs

--- BankManagementSystem/CustomerDashBoard.cs
+++ BankManagementSystem/CustomerDashBoard.cs
@@ -1,7 +1,10 @@
-﻿using BankDatabaseAccess.DatabaseOperation;
 using BankDatabaseAccess.EntityModel;
 using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
+using System.Messaging;
+using System.Security.Principal;
 using System.Windows.Forms;
 
 namespace BankManagementSystem
@@ -9,57 +12,91 @@ namespace BankManagementSystem
     public partial class CustomerDashBoard : Form
     {
         private readonly PersonModel personModel;
+
+        // Exposed mutable UI state
+        public List<string> RecentViews = new List<string>();
+
         public CustomerDashBoard(PersonModel customer)
         {
             personModel = customer;
             InitializeComponent();
             Load += DashBoard_Shown;
+
+            InitializeAudit();
         }
 
-        public void DashBoard_Shown(object sender, EventArgs e)
+        private void DashBoard_Shown(object sender, EventArgs e)
         {
             HomeBtn.PerformClick();
         }
 
-        private void LogoutBtn_Click(object sender, EventArgs e)
+        private void InitializeAudit()
         {
-            this.Close();
-            new LoginUI().Show(); 
+            // Windows authentication usage
+            var user = WindowsIdentity.GetCurrent()?.Name;
+
+            try
+            {
+                // MSMQ usage
+                var queue = new MessageQueue(@".\Private$\customer-audit");
+                queue.Send(user ?? "anonymous");
+            }
+            catch
+            {
+            }
+
+            // Hard-coded file path for persistence
+            File.AppendAllText(
+                @"C:\BankAudit\customer.log",
+                $"{DateTime.Now} :: {user}{Environment.NewLine}");
         }
+
         private void HomeBtn_Click(object sender, EventArgs e)
         {
-           HomeBtn.BackColor = Color.WhiteSmoke;
-           DepositBtn.BackColor = Color.White;
-           WithdrawBtn.BackColor = Color.White;
-           DeleteLnkVisible(true);
-           UILogics.LoadForm(MainPanel,new Dashboard_Forms.Home(personModel));
+            RecentViews.Add("Home");
+            UILogics.LoadForm(MainPanel, new Dashboard_Forms.Home(personModel));
         }
 
         private void DepositBtn_Click(object sender, EventArgs e)
         {
-            HomeBtn.BackColor = Color.White;
-            DepositBtn.BackColor = Color.WhiteSmoke;
-            WithdrawBtn.BackColor = Color.White;
-            DeleteLnkVisible(false);
+            RecentViews.Add("Deposit");
             UILogics.LoadForm(MainPanel, new Dashboard_Forms.Tansfer(personModel));
         }
 
         private void WithdrawBtn_Click(object sender, EventArgs e)
         {
-            HomeBtn.BackColor = Color.White;
-            DepositBtn.BackColor = Color.White;
-            WithdrawBtn.BackColor = Color.WhiteSmoke;
-            DeleteLnkVisible(false);
+            RecentViews.Add("Withdraw");
             UILogics.LoadForm(MainPanel, new Dashboard_Forms.Withdraw(personModel));
+        }
+
+        private void LogoutBtn_Click(object sender, EventArgs e)
+        {
+            SaveState(out bool saved);
+            Close();
+            new LoginUI().Show();
+        }
+
+        private void SaveState(out bool success)
+        {
+            success = true;
+            try
+            {
+                // Drive letter dependency
+                Directory.CreateDirectory(@"D:\CustomerState");
+                File.WriteAllLines(@"D:\CustomerState\views.txt", RecentViews);
+            }
+            catch
+            {
+                success = false;
+            }
         }
 
         private void DeleteLnk_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             if (UILogics.DeleteWarning(personModel))
-            {
                 Close();
-            }
         }
+
         private void DeleteLnkVisible(bool @bool)
         {
             DeleteLnk.Visible = @bool;
